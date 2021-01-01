876c1afe5b81239e54285434e7b8f3285b548de2
Oliver Löffler <22102800+Oliver-Loeffler@users.noreply.github.com>
2021-01-01 20:19:40 +0100

    JUnit API import added.

--- src/test/java/net/raumzeitfalle/xml/XmlValidatorTest.java
+++ src/test/java/net/raumzeitfalle/xml/XmlValidatorTest.java
@@ -11,6 +11,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
